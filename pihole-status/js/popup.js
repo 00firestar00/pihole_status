d02443399ed64568776265b5a04d83e6b0e8d78a
Evan Jarrett <ejfirestar00@gmail.com>
2018-02-06 00:17:45 -0800

    major rewrite. uses material lite instead of bootstrap, change icon to match latest pihole icon, swapped to using api token. but still an issues with CORS

--- pihole-status/js/popup.js
+++ pihole-status/js/popup.js
@@ -1,75 +1,87 @@
-var apiurl;
+var DEFAULT_API_URL = "http://pihole/admin",
+    apiURL = document.getElementById("apiURL"),
+    apiToken = document.getElementById("apiToken"),
+    piholeStatus = document.getElementById("piholeStatus"),
+    endpoint = "/api.php";
 
+function save_url() {
+    console.log("saving: " + this.value);
+    chrome.storage.sync.set({
+        apiURL: this.value
+    }, function() {
+        console.log("Saved");
+    });
+}
+
+function save_token() {
+    console.log("saving: " + this.value);
+    chrome.storage.sync.set({
+        apiToken: this.value
+    }, function() {
+        console.log("Saved");
+    });
+}
+
+// Localstorage
 function restore_options() {
-  // Use defaults if empty
-  chrome.storage.sync.get({
-    apiurl: 'http://192.168.178.63/admin/apiext.php'
-  }, function(items) {
-    document.getElementById('apiurl').value = items.apiurl;
-  });
+    // Use defaults
+    chrome.storage.sync.get({
+        apiURL: DEFAULT_API_URL,
+        apiToken: '000-000-000'
+    }, function(items) {
+        apiURL.value = items.apiURL;
+        apiToken.value = items.apiToken;
+    });
 }
 
 function toggleState() {
-	var apiurl = document.getElementById('apiurl').value
-	var state = $('#status').bootstrapSwitch('state');
-	if (!state){
-		apiurl = apiurl + "?disable";
-	} else {
-		apiurl = apiurl + "?enable";
-	}
-	$.ajax({
-      type:'POST',
-      url: apiurl,
-    })    
-	.done(function(data) {
-		if($('#status').bootstrapSwitch('state')){
-			$('form').append('<div id="alert" name="alert" class="alert alert-success">Pi-Hole enabled</div>').hide().fadeIn("slow"); 
-		} else {
-			$('form').append('<div id="alert" name="alert" class="alert alert-success">Pi-Hole disabled</div>').hide().fadeIn("slow"); 
-		}
-			$('#alert').delay(2000).fadeOut(5000, function() {
-			  $(this).remove();
-			});    
-    })
-    .fail(function(data) {
-		$('form').append('<div id="alert" name="alert" class="alert alert-danger">Pi-Hole state toggling was not succesful</div>' + apiurl).hide().fadeIn("slow"); 
-		$('#alert').delay(2000).fadeOut(5000, function() {
-		  $(this).remove();
-		});    
-         console.log(data);
-    });
-
-}
-function check_status(){
-	var apiurl = document.getElementById('apiurl').value    
-
-	$.ajax({
-      type:'GET',
-      url: apiurl + "?status",
-    })    
-	.done(function(data) {
-		$('#status').bootstrapSwitch('toggleDisabled');
-        if ( data.status == 1) {
-			$('#status').bootstrapSwitch('state', true, true);			
-		} else {
-			$('#status').bootstrapSwitch('state', false, true);			
-		}
-    })
-    .fail(function(data) {
-		$('form').append('<div id="alert" name="alert" class="alert alert-danger">Pi-Hole state toggling was not succesful</div>').hide().fadeIn("slow"); 
-		$('#alert').delay(2000).fadeOut(5000, function() {
-		  $(this).remove();
-		});    
-         console.log(data);
-    });
-	
+    var apiurl = apiURL.value + endpoint,
+        auth = apiToken.value;
+    if (!piholeStatus.checked) {
+        apiurl += "?disable&auth=" + auth;
+    } else {
+        apiurl += "?enable&auth=" + auth;
+    }
+    $.ajax({
+            type: 'POST',
+            url: apiurl,
+        })
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function(data) {
+            console.log(data);
+        });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	$('#status').bootstrapSwitch();	
-	restore_options();
-	window.setTimeout(check_status,1000);
-	$('#status').on('switchChange.bootstrapSwitch', function (event, state) {		
-		toggleState();
-	});
-});
+
+apiURL.addEventListener('input', save_url);
+apiToken.addEventListener('input', save_token);
+piholeStatus.addEventListener('change', toggleState);
+document.addEventListener('DOMContentLoaded', restore_options);
+
+
+function check_status() {
+    var url = apiURL.value
+
+    $.ajax({
+            type: 'GET',
+            url: url + "?status",
+        })
+        .done(function(data) {
+            $('#status').bootstrapSwitch('toggleDisabled');
+            if (data.status == 1) {
+                $('#status').bootstrapSwitch('state', true, true);
+            } else {
+                $('#status').bootstrapSwitch('state', false, true);
+            }
+        })
+        .fail(function(data) {
+            $('form').append('<div id="alert" name="alert" class="alert alert-danger">Pi-Hole state toggling was not succesful</div>').hide().fadeIn("slow");
+            $('#alert').delay(2000).fadeOut(5000, function() {
+                $(this).remove();
+            });
+            console.log(data);
+        });
+
+}
